832b3b578000b43087e9128eda766c6d3aa099f2
Umar Baba Umar <omexx52@gmail.com>
2023-09-07 04:55:43 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - this is the main func
+ * main - this is the main function
  *
  * Return this is always 0
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
