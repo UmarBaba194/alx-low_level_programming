6620279939ee5e10dfdb2f21d937721fd4c1c9cf
Umar Baba Umar <omexx52@gmail.com>
2023-09-12 12:22:45 -0700

    7 main

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_last_digit - prints the last digit of a number.
+ * print_last_digit - prints the last digit of a numer.
  * @n: digit to find the last place of.
  * Return: The last digit.
  */
