ad695aa5c8a1f4eacfcc89581fe008c2bd75b126
Umar Baba Umar <omexx52@gmail.com>
2023-09-12 13:48:52 -0700

    last didgit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,12 @@
  */
 int print_last_digit(int n)
 {
+	int k;
+
+	k = n % 10;
 	if (n < 0)
-	{
-		n = n * -1;
-	}
-	_putchar((n % 10) + '0');
-	return (n % 10);
+		k = -k;
+	_putchar(k + '0');
+	return (k);
+
 }
