34a6ae5293abb1a4075b585b0e0c54228eddc844
Umar Baba Umar <omexx52@gmail.com>
2023-10-19 23:14:10 -0700

    m

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LIST_H
+#define LIST_H
 
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,6 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -21,6 +23,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void before_main() __attribute__((constructor));
+int _putchar(char c);
 
 #endif
